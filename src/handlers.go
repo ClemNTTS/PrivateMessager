55032f0441ab347c6017dc47e6b927ecbe2db02f
cnuttens <clement.nutt@gmail.com>
2024-09-26 15:47:50 +0200

    Need to link the fcking css

--- src/handlers.go
+++ src/handlers.go
@@ -1,1 +1,25 @@
 package prvMessager
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var(
+	index, err = template.ParseFiles("./web/templates/index.html")
+)
+
+func ConnectionHandler(w http.ResponseWriter, r *http.Request) {
+
+	if err != nil{
+		log.Fatal(err)
+	}
+
+
+	if r.URL.Path == "/"{
+		index.Execute(w,nil)
+	}else{
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
--- src/serv.go
+++ src/serv.go
@@ -1,1 +1,39 @@
 package prvMessager
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+//Launch a server on the following port given in program argument
+func Launch(){
+	port := "8080"
+	if len(os.Args) == 2 && len(os.Args[1]) == 4{
+		port = os.Args[1]
+		if _, err := strconv.Atoi(port); err != nil || len(port) < 2 || len(port) > 5 {
+			log.Fatal("Invalid port number")
+	}
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", ConnectionHandler)
+
+	server := &http.Server{
+		Addr: ":"+port,
+		Handler: mux,
+		MaxHeaderBytes: 1 <<20, //1Mb
+		IdleTimeout: 120 * time.Second,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ReadHeaderTimeout: 10 * time.Second,
+	}
+
+	log.Println("Listening on http://localhost"+server.Addr)
+	if err := server.ListenAndServe(); err != nil{
+		log.Fatal(err)
+	}
+}
